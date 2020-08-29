25b623895b09e303832cfc59ad8261255b834057
keita <keitahino0415@gmail.com>
2020-08-29 11:28:36 +0900

    composition api導入

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import store from './store'
 import VueAxios from 'vue-axios'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueCompositionApi from "@vue/composition-api";
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
+Vue.use(VueCompositionApi);
 
 new Vue({
   router,
